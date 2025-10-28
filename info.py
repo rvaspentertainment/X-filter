fc2c6d9545ce9e30470213dde270fdade7f24e6a
rvaspentertainment <112102231+rvaspentertainment@users.noreply.github.com>
2025-10-28 21:59:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ BOT_USERNAME = environ.get("BOT_USERNAME", '')  # Bot's username
 
 # 🔗 Channel & Support Links
 CHANNEL = environ.get('CHANNEL', '')  # Updates channel
-SUPPORT = environ.get('SUPPORT', 't.me')  # Support group
+SUPPORT = environ.get('SUPPORT', '')  # Support group
 HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 't.me')  # Verification guide link
 HOW_TO_OPEN = environ.get('HOW_TO_OPEN', 't.me')  # File access guide link
 
